8e55aff1f0f5612b21c6149ce0a3646f33f7e033
Nikola Bjelica <100382766+NikolaBjelica@users.noreply.github.com>
2023-06-26 20:05:27 -0400

    Added colors based on colorhunt.co color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:
+      {
+        primary: '#C2DEDC',
+        secondary: '#ECE5C7',
+        third: '#CDC2AE',
+        fourth: '#116A7B'
+      }
     },
   },
   plugins: [],
