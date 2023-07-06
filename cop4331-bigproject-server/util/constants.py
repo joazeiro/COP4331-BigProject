61e4aad8e0b37fceb0e5e88b28497c8091e86dcf
joazeiro <97628791+joazeiro@users.noreply.github.com>
2023-07-06 12:49:55 -0400

    added constants for the email verification

--- cop4331-bigproject-server/util/constants.py
+++ cop4331-bigproject-server/util/constants.py
@@ -1,9 +1,13 @@
 import pymongo
 import sys
 
+
 class Keys:
     DB_URI = "mongodb+srv://cop4331group:SzR3ycsGck2W9PzS@bigprojectdb.39szjld.mongodb.net/" # once we set up venv this will be hidden
     DB_NAME = "BigProjectDatabase"
     COLLECTION_USER = "userDatabase"
     COLLECTION_REGION = "regionDatabase"
-    SECRET_KEY = "ourcountryprojectforcop4331".encode('utf-8')
+    SECRET_KEY = "ourcountryprojectforcop4331"
+    MAIL_USERNAME = "noreply.geobook@gmail.com"
+    MAIL_PASSWORD = "Oitudobem10" 
+    MAIL_SERVER = "smtp.gmail.com"
