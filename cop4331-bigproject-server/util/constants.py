6c9fef3c57a0fac7eb32d0bc534dd41318ea1c0f
Greesh12 <134640893+Greesh12@users.noreply.github.com>
2023-07-06 23:12:43 -0400

    Update constants.py

--- cop4331-bigproject-server/util/constants.py
+++ cop4331-bigproject-server/util/constants.py
@@ -1,12 +1,11 @@
 import pymongo
 import sys
 
-
 class Keys:
     DB_URI = "mongodb+srv://cop4331group:SzR3ycsGck2W9PzS@bigprojectdb.39szjld.mongodb.net/" # once we set up venv this will be hidden
     DB_NAME = "BigProjectDatabase"
     COLLECTION_USER = "userDatabase"
-    COLLECTION_REGION = "regionDatabase"
+    COLLECTION_POST = "postDatabase"
     SECRET_KEY = "ourcountryprojectforcop4331"
     MAIL_USERNAME = "noreply.geobook@gmail.com"
     MAIL_PASSWORD = "Oitudobem10" 
