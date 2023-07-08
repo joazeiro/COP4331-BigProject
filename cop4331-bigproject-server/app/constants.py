b9031a61863c29e9e23198906a66f17ded3ae52e
Greesh12 <134640893+Greesh12@users.noreply.github.com>
2023-07-08 15:23:27 -0400

    Add files via upload

---
+++ cop4331-bigproject-server/app/constants.py
@@ -0,0 +1,12 @@
+import pymongo
+import sys
+
+class Keys:
+    DB_URI = "mongodb+srv://cop4331group:SzR3ycsGck2W9PzS@bigprojectdb.39szjld.mongodb.net/" # once we set up venv this will be hidden
+    DB_NAME = "BigProjectDatabase"
+    COLLECTION_USER = "userDatabase"
+    COLLECTION_POST = "postDatabase"
+    SECRET_KEY = "ourcountryprojectforcop4331"
+    MAIL_USERNAME = "noreply.geobook@gmail.com"
+    MAIL_PASSWORD = "enxwxepabtwzuusy" 
+    MAIL_SERVER = "smtp.gmail.com"
